fa36fe3fe90ec39cd5c7bc2857de320c36825de6
andres620 <andres.1059814779@gmail.com>
2019-10-20 12:58:37 -0500

    Creamos el proyecto

---
+++ graph.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 12:49:31 2019
+
+@author: ayuwoki
+"""
+from heapq import heappop, heappush
+
+class graph:
+    def __init__(self,gr):
+        self.graph=gr    
