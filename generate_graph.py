7252c64521a1855a20c43eac617badb705348837
andres620 <andres.1059814779@gmail.com>
2019-10-22 20:52:54 -0500

    Algoritmo de generate graph casi completo, falta validar que al grafo no se añadan los datos que ya estan

--- generate_graph.py
+++ generate_graph.py
@@ -18,68 +18,104 @@ class generate_graph:
             self.oveja['estado_inicial'].append([0,0,0,0])
             self.oveja['estado_aceptacion'].append([1,1,1,1])
         
-            self.oveja['graph'].append({'label':self.oveja['estado_inicial'],'FIN':False,
-                                   'adyacentes':[]})
+            self.oveja['graph'].append({'label':[0,0,0,0],'FIN':False,
+                                   'adyacentes':[], 'done':True})
             
-            self.generate_sheep_r()
+            self.generate_sheep_r(self.oveja['graph'][0])
     
             with open('oveja.json', 'a') as outfile:
                 json.dump(self.oveja, outfile)
                 
-    def generate_sheep_r(self):
+    def generate_sheep_r(self,h):
+            print('what?', h,'\n')
+            
+            #condicion de escape
+            for k in  self.oveja['graph']:
+                if k['label']==[1,1,1,1]:
+                    return
         
-        for h in self.oveja['graph']:
-            
-            
             #pasar repollo
-            if h['label'][0][0]==0 and h['label'][0][3]==0:
-                aux=h['label'][0][:]
-                print("que e au?:",aux)
+            if h['label'][0]==0 and h['label'][3]==0:
+                aux=h['label'][:]
                 aux[0]=1
                 aux[3]=1
-                if h['label'][0][1]==h['label'][0][2]:
+                if h['label'][1]==h['label'][2]:
                     h['adyacentes'].append({'label':aux , 'FIN':True})
                 else:
                     h['adyacentes'].append({'label':aux , 'FIN':False})
-            #pasar obeja
-            if h['label'][0][1]==0 and h['label'][0][3]==0:
-                aux=h['label'][0][:]
+            #pasar oveja
+            if h['label'][1]==0 and h['label'][3]==0:
+                aux=h['label'][:]
                 aux[1]=1
                 aux[3]=1
                 h['adyacentes'].append({'label':aux , 'FIN':False})
             
             #pasar lobo
-            if h['label'][0][2]==0 and h['label'][0][3]==0:
-                aux=h['label'][0][:]
+            if h['label'][2]==0 and h['label'][3]==0:
+                aux=h['label'][:]
                 aux[2]=1
                 aux[3]=1
-                if h['label'][0][0]==h['label'][0][1]:
+                if h['label'][0]==h['label'][1]:
                     h['adyacentes'].append({'label':aux , 'FIN':True})
                 else:
                     h['adyacentes'].append({'label':aux , 'FIN':False})
             #pasar solo barco
-            if h['label'][0][3]==0:
-                aux=h['label'][0][:]
+            if h['label'][3]==0:
+                aux=h['label'][:]
                 aux[3]=1
-                if h['label'][0][0]==h['label'][0][1] or h['label'][0][1]==h['label'][0][2]:
+                if h['label'][0]==h['label'][1] or h['label'][1]==h['label'][2]:
                     h['adyacentes'].append({'label':aux , 'FIN':True})
                 else:
                     h['adyacentes'].append({'label':aux , 'FIN':False})
             
             #devolver repollo
-            if h['label'][0][0]==1 and h['label'][0][3]==1:
-                aux=h['label'][0][:]
+            if h['label'][0]==1 and h['label'][3]==1:
+                aux=h['label'][:]
                 aux[0]=0
                 aux[3]=0
-                if h['label'][0][1]==h['label'][0][2]:
+                if h['label'][1]==h['label'][2]:
                     h['adyacentes'].append({'label':aux , 'FIN':True})
                 else:
                     h['adyacentes'].append({'label':aux , 'FIN':False})
+            
+            #devolver oveja
+            if h['label'][1]==1 and h['label'][3]==1:
+                aux=h['label'][:]
+                aux[1]=0
+                aux[3]=0
+                h['adyacentes'].append({'label':aux , 'FIN':False})
+            
+            #devolver lobo
+            if h['label'][2]==1 and h['label'][3]==1:
+                aux=h['label'][:]
+                aux[2]=0
+                aux[3]=0
+                if h['label'][0]==h['label'][1]:
+                    h['adyacentes'].append({'label':aux , 'FIN':True})
+                else:
+                    h['adyacentes'].append({'label':aux , 'FIN':False})
+                
+            #pasar solo barco
+            if h['label'][3]==1:
+                aux=h['label'][:]
+                aux[3]=0
+                if h['label'][0]==h['label'][1] or h['label'][1]==h['label'][2]:
+                    h['adyacentes'].append({'label':aux , 'FIN':True})
+                else:
+                    h['adyacentes'].append({'label':aux , 'FIN':False})
+            
+            
+                        
+        
+            for j in h['adyacentes']:
+                self.oveja['graph'].append({'label':j['label'],'FIN':j['FIN'],
+                                   'adyacentes':[], 'done': False})
 
-                
-                
-                
-                
+            for i in self.oveja['graph']:
+                if not i['done']:
+                    i['done']=True
+                    if not i['FIN']:
+                        self.generate_sheep_r(i)
                 
                 
         
