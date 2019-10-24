bd2d9f980a96d224edc3a83ed6552d7e78395bae
Lsjuandiego <juan.1701624716@ucaldas.edu.co>
2019-10-24 15:51:24 -0500

    falta validar que no se desborde la lista de ranas

--- generate_graph.py
+++ generate_graph.py
@@ -9,13 +9,95 @@ class generate_graph:
     
     def __init__(self,ov):
         self.oveja=ov
+        self.frog = ov
         
     def in_graph(self,x):
         for h in self.oveja['graph']:
             if x==h['label']:
                 return True
         return False
+    def in_graph_sapo(self,x):
+        for h in self.frog['graph']:
+            if x==h['label']:
+                return True
+        return False
     
+    def generate_frog(self):
+            self.frog['estado_inicial'] =[]
+            self.frog['estado_aceptacion'] =[]
+            self.frog['graph']=[]
+            self.frog['transiciones'] = []
+            self.frog['estado_inicial'].append([1,1,1,0,2,2,2])
+            self.frog['estado_aceptacion'].append([2,2,2,0,1,1,1])
+            self.frog['graph'].append({'label':[1,1,1,0,2,2,2],'FIN':False,
+                                   'adyacentes':[], 'done':True})
+            
+            self.generate_frog_r(self.frog['graph'][0])
+    
+            with open('rana.json', 'a') as outfile:
+                json.dump(self.frog, outfile)
+            
+            
+    def generate_frog_r(self,h):
+            print('what?', h,'\n')
+            #condicion de escape
+            for k in  self.frog['graph']:
+                if k['label']==[2,2,2,0,1,1,1]:
+                    return
+              
+            #mover la piedra una casilla a la izquierda
+            for k in self.frog['graph']:
+                for i in range(len(k['label'])):
+                    if k['label'][i] == 0:
+                        if h['label'][i-1]==1 and (0 <= i-1 <= len(h['label'])):
+                            aux=h['label'][:]
+                            aux[i]=1
+                            aux[i-1]=0
+                            if h['label'][i] == 0 and h['label'][i-1] == 2 and h['label'][i+1] == 1:
+                                h['adyacentes'].append({'label':aux , 'FIN':True})
+                            else:
+                                h['adyacentes'].append({'label':aux , 'FIN':False})
+            #mover la piedra una casilla a la derecha
+                        if h['label'][i+1]==2 and (0 <= i+1 <= len(h['label'])):
+                            aux=h['label'][:]
+                            aux[i]=2
+                            aux[i+1]=0
+                            if h['label'][i] == 0 and h['label'][i+1] == 1 and h['label'][i-1] == 2:
+                                h['adyacentes'].append({'label':aux , 'FIN':True})
+                            else:
+                                h['adyacentes'].append({'label':aux , 'FIN':False})
+            
+            #mover piedra dos casillas a la izquierda
+                        if h['label'][i-2]==1 and h['label'][i-1] == 2 and (0 <= i-2 <= len(h['label'])):
+                            aux=h['label'][:]
+                            aux[i]=1
+                            aux[i-2]=0
+                            h['adyacentes'].append({'label':aux , 'FIN':False})
+                        print("lfjekhfij",i)    
+            #mover piedra dos casillas a la derecha
+                        if h['label'][i+2]==2 and h['label'][i+1] == 1 and (0 <= i+2 <= len(h['label'])):
+                            aux=h['label'][:]
+                            aux[i]=2
+                            aux[i+2]=0
+                            h['adyacentes'].append({'label':aux , 'FIN':False})
+            
+            
+            for j in h['adyacentes']:
+                if self.in_graph_sapo(j['label']):
+                    continue
+                else:
+                    self.frog['graph'].append({'label':j['label'],'FIN':j['FIN'],
+                                   'adyacentes':[], 'done': False})
+    
+
+            for i in self.frog['graph']:
+                if not i['done']:
+                    i['done']=True
+                    if not i['FIN']:
+                        self.generate_frog_r(i)
+                
+    
+                            
     def generate_sheep(self):
             self.oveja['estado_inicial'] =[]
             self.oveja['estado_aceptacion'] =[]
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ Created on Sun Oct 20 15:08:08 2019
 import json
 from generate_graph import generate_graph
 def main():
-
-  #  with open('oveja.json','r') as file:
-       # data =json.load(file)
-        #aux=data['dominio'].append({'nombre':'jose jose'})
-        #print(aux)
-    oveja = {}
-    gr=generate_graph(oveja)
-    gr.generate_sheep()
+    #oveja = {}
+    #gr=generate_graph(oveja)
+    #gr.generate_sheep()
+    sapo = {}
+    
+    gr = generate_graph(sapo)
+ 
+    gr.generate_frog()
     
 
 if __name__== "__main__":
